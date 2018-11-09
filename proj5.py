168aee22fb5a8c574c216520b35be0e9daf18d01
aviraj-sinha <avirajs@smu.edu>
2018-11-09 17:34:32 -0600

    Added wide and deep

--- proj5.py
+++ proj5.py
@@ -10,17 +10,16 @@ n = sum(1 for line in open(filename)) - 1 #number of records in file (excludes h
 s = 1000 #desired sample size
 skip = sorted(rd.sample(range(1,n+1),n-s)) #the 0-indexed header will not be included in the skip list
 bike_rides = pd.read_csv(filename, skiprows=skip)
-
+print(bike_rides.shape)
+#remove trip duation and make as the y variable categorical label, coordingates, usetypes were all subscriber, times were already give, station name already has id,
+bike_rides = bike_rides.drop(columns=['trip_id','year','latitude_end', 'longitude_end', 'latitude_start', 'longitude_start', 'usertype', 'starttime','stoptime','from_station_name','to_station_name'])
 bike_rides.head()
-bike_rides.shape
 
 bike_rides.tripduration.max()
 labels = ["{0} - {1}".format(i, i + 4) for i in range(0, 60, 5)]
-bike_rides['group_duration'] = pd.cut(bike_rides.tripduration, range(0, 65, 5), right=False, labels=labels)
-bike_rides[['group_duration','tripduration']]
 
 
-bike_rides['usertype'].max()
+bike_rides['tripduration'].max()
 
 
 bike_rides['gender'].value_counts()
@@ -30,43 +29,54 @@ bike_rides['gender'].value_counts()
 
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
-sns.countplot(x="group_duration", data=bike_rides)
+sns.distplot(a=bike_rides["tripduration"])
 plt.show()
 
-
-#remove trip duation and make as the y variable categorical label, coordingates, usetypes were all subscriber, times were already give, station name already has id,
-bike_rides = bike_rides.drop(columns=['trip_id','year','tripduration','latitude_end', 'longitude_end', 'latitude_start', 'longitude_start', 'usertype', 'starttime','stoptime','from_station_name','to_station_name'])
-
 list(bike_rides.columns)
-
-# X = bike_rides.drop(columns=["group_duration"])
-# y = bike_rides["group_duration"]
-# n_samples, n_features = X.shape
-# n_classes = len(np.unique(y))
-# print("n_samples: {}".format(n_samples))
-# print("n_features: {}".format(n_features))
-# print("n_classes: {}".format(n_classes))
-# print(np.unique(y))
+bike_rides.loc[bike_rides['tripduration'] < 10,'group_duration'] = "LOW"
+bike_rides.loc[bike_rides['tripduration'] > 10,'group_duration'] = "HIGH"
+bike_rides.head()
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 
+
+# bike_rides[categorical_headers] = bike_rides[categorical_headers].astype(str)
+categorical_headers = ['month','week','day','hour','gender','events']
 bike_rides[categorical_headers] = bike_rides[categorical_headers].astype(str)
 
 df_train, df_test = train_test_split(bike_rides, test_size=0.2)
+df_train.dropna(inplace=True)
+df_train.reset_index()
 
-
+df_test.dropna(inplace=True)
+df_test.reset_index()
+print(df_test.group_duration.value_counts())
 
 #,'from_station_id','to_station_id'
 encoders = dict()
-categorical_headers = ['month','week','day','hour','gender','events']
 
 #dp is the total number of docs at each station
+
+for col in categorical_headers + ['group_duration'] :
+    df_train[col] = df_train[col].str.strip()
+    df_test[col] = df_test[col].str.strip()
+
+    if col=="group_duration":
+        tmp = LabelEncoder()
+        df_train[col] = tmp.fit_transform(df_train[col])
+        df_test[col] = tmp.transform(df_test[col])
+    else:
+        # integer encoded variables
+        encoders[col] = LabelEncoder()
+        df_train[col+'_int'] = encoders[col].fit_transform(df_train[col])
+        df_test[col+'_int'] = encoders[col].transform(df_test[col])
+
 
 # scale the numeric, continuous variables
 numeric_headers = ['temperature','dpcapacity_start','dpcapacity_end']
 
-for col in numeric_headers:
+for col in numeric_headers :
     df_train[col] = df_train[col].astype(np.float)
     df_test[col] = df_test[col].astype(np.float)
 
@@ -74,22 +84,7 @@ for col in numeric_headers:
     df_train[col] = ss.fit_transform(df_train[col].values.reshape(-1, 1))
     df_test[col] = ss.transform(df_test[col].values.reshape(-1, 1))
 
-
-
-
-bike_rides.info()
-
-
-for col in categorical_headers + ['group_duration']:
-    df_train[col] = df_train[col].str.strip()
-    df_test[col] = df_test[col].str.strip()
-
-    # integer encoded variables
-    encoders[col] = LabelEncoder() # save the encoder
-    df_train[col+'_int'] = encoders[col].fit_transform(df_train[col])
-    df_test[col+'_int'] = encoders[col].transform(df_test[col])
-
-
+df_test.head()
 categorical_headers_ints = [x+'_int' for x in categorical_headers]
 
 # we will forego one-hot encoding right now and instead just scale all inputs
@@ -98,8 +93,93 @@ feature_columns = categorical_headers_ints+numeric_headers
 X_train =  ss.fit_transform(df_train[feature_columns].values).astype(np.float32)
 X_test =  ss.transform(df_test[feature_columns].values).astype(np.float32)
 
-y_train = df_train['group_duration'].values.astype(np.str)
-y_test = df_test['group_duration'].values.astype(np.str)
+y_train = df_train['group_duration'].values.astype(np.int)
+y_test = df_test['group_duration'].values.astype(np.int)
+
+print(feature_columns)
+
+df_train[feature_columns].head()
+df_train["group_duration"].head()
+
+
+
+
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+
+# bike_rides[categorical_headers] = bike_rides[categorical_headers].astype(str)
+categorical_headers = ['month','week','day','hour','gender','events']
+bike_rides[categorical_headers] = bike_rides[categorical_headers].astype(str)
+
+df_train, df_test = train_test_split(bike_rides, test_size=0.2)
+df_train.dropna(inplace=True)
+df_train.reset_index()
+
+df_test.dropna(inplace=True)
+df_test.reset_index()
+print(df_test.group_duration.value_counts())
+
+#,'from_station_id','to_station_id'
+encoders = dict()
+
+#dp is the total number of docs at each station
+
+for col in categorical_headers + ['group_duration'] :
+    df_train[col] = df_train[col].str.strip()
+    df_test[col] = df_test[col].str.strip()
+
+    if col=="group_duration":
+        tmp = LabelEncoder()
+        df_train[col] = tmp.fit_transform(df_train[col])
+        df_test[col] = tmp.transform(df_test[col])
+    else:
+        # integer encoded variables
+        encoders[col] = LabelEncoder()
+        df_train[col+'_int'] = encoders[col].fit_transform(df_train[col])
+        df_test[col+'_int'] = encoders[col].transform(df_test[col])
+
+
+# scale the numeric, continuous variables
+numeric_headers = ['temperature','dpcapacity_start','dpcapacity_end']
+
+for col in numeric_headers :
+    df_train[col] = df_train[col].astype(np.float)
+    df_test[col] = df_test[col].astype(np.float)
+
+    ss = StandardScaler()
+    df_train[col] = ss.fit_transform(df_train[col].values.reshape(-1, 1))
+    df_test[col] = ss.transform(df_test[col].values.reshape(-1, 1))
+
+df_test.head()
+
+
+
+
+
+from sklearn.preprocessing import OneHotEncoder
+categorical_headers_ints = [x+'_int' for x in categorical_headers]
+
+# we will forego one-hot encoding right now and instead just scale all inputs
+#   this is just to get an example running in Keras (don't ever do this)
+feature_columns = categorical_headers_ints+numeric_headers
+X_train =  ss.fit_transform(df_train[feature_columns].values).astype(np.float32)
+X_test =  ss.transform(df_test[feature_columns].values).astype(np.float32)
+
+y_train = df_train['group_duration'].values.astype(np.int)
+y_test = df_test['group_duration'].values.astype(np.int)
+
+
+ohe = OneHotEncoder()
+X_train_ohe = ohe.fit_transform(df_train[categorical_headers_ints].values)
+X_test_ohe = ohe.transform(df_test[categorical_headers_ints].values)
+
+# and save off the numeric features
+X_train_num =  df_train[numeric_headers].values
+X_test_num = df_test[numeric_headers].values
 
 print(feature_columns)
 
@@ -109,36 +189,237 @@ print(feature_columns)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 from sklearn import metrics as mt
 import keras
-
+from keras import regularizers
 keras.__version__
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Input
+from keras.layers import Dense, Activation, Input, Dropout
 from keras.layers import Embedding, Flatten, Concatenate
+from keras.layers import concatenate
 from keras.models import Model
 
-# combine the features into a single large matrix
-X_train = df_train[feature_columns].values
-X_test = df_test[feature_columns].values
 
-# This returns a tensor
-inputs = Input(shape=(X_train.shape[1],))
 
-# a layer instance is callable on a tensor, and returns a tensor
-x = Dense(units=10, activation='relu')(inputs)
-predictions = Dense(1,activation='sigmoid')(x)
 
-# This creates a model that includes
-# the Input layer and three Dense layers
-model = Model(inputs=inputs, outputs=predictions)
+from sklearn.preprocessing import OneHotEncoder
 
-model.compile(optimizer='sgd',
-              loss='mean_squared_error',
+cross_columns = [['month','week','day'],['events','hour']]
+
+
+#'workclass','education','marital_status','occupation','relationship','race','sex','country'
+
+# we need to create separate lists for each branch
+embed_branches = []
+X_ints_train = []
+X_ints_test = []
+all_inputs = []
+all_wide_branch_outputs = []
+
+for cols in cross_columns:
+    # encode crossed columns as ints for the embedding
+    enc = LabelEncoder()
+
+    # create crossed labels
+    X_crossed_train = df_train[cols].apply(lambda x: '_'.join(x), axis=1)
+    X_crossed_test = df_test[cols].apply(lambda x: '_'.join(x), axis=1)
+
+    enc.fit(np.hstack((X_crossed_train.values,  X_crossed_test.values)))
+    X_crossed_train = enc.transform(X_crossed_train)
+    X_crossed_test = enc.transform(X_crossed_test)
+    X_ints_train.append( X_crossed_train )
+    X_ints_test.append( X_crossed_test )
+
+    # get the number of categories
+    N = max(X_ints_train[-1]+1) # same as the max(df_train[col])
+
+    # create embedding branch from the number of categories
+    inputs = Input(shape=(1,),dtype='int32', name = '_'.join(cols))
+    all_inputs.append(inputs)
+    x = Embedding(input_dim=N,
+                  output_dim=int(np.sqrt(N)),
+                  input_length=1, name = '_'.join(cols)+'_embed')(inputs)
+    x = Flatten()(x)
+    all_wide_branch_outputs.append(x)
+
+# merge the branches together
+wide_branch = concatenate(all_wide_branch_outputs, name='wide_concat')
+wide_branch = Dense(units=1,activation='sigmoid',name='wide_combined')(wide_branch)
+
+# reset this input branch
+all_deep_branch_outputs = []
+# add in the embeddings
+for col in categorical_headers_ints:
+    # encode as ints for the embedding
+    X_ints_train.append( df_train[col].values )
+    X_ints_test.append( df_test[col].values )
+
+    # get the number of categories
+    N = max(X_ints_train[-1]+1) # same as the max(df_train[col])
+
+    # create embedding branch from the number of categories
+    inputs = Input(shape=(1,),dtype='int32', name=col)
+    all_inputs.append(inputs)
+    x = Embedding(input_dim=N,
+                  output_dim=int(np.sqrt(N)),
+                  input_length=1, name=col+'_embed')(inputs)
+    x = Flatten()(x)
+    all_deep_branch_outputs.append(x)
+
+# also get a dense branch of the numeric features
+all_inputs.append(Input(shape=(X_train_num.shape[1],),
+                        sparse=False,
+                        name='numeric_data'))
+
+x = Dense(units=20, activation='relu',name='numeric_1')(all_inputs[-1])
+all_deep_branch_outputs.append( x )
+
+# merge the deep branches together
+deep_branch = concatenate(all_deep_branch_outputs,name='concat_embeds')
+deep_branch = Dense(units=50,kernel_regularizer=regularizers.l2(0.01), activation='relu', name='deep1')(deep_branch)
+deep_branch = Dropout(0.5)(deep_branch)
+deep_branch = Dense(units=25,kernel_regularizer=regularizers.l2(0.01), activation='relu', name='deep2')(deep_branch)
+deep_branch = Dropout(0.5)(deep_branch)
+deep_branch = Dense(units=10,kernel_regularizer=regularizers.l2(0.01), activation='relu', name='deep3')(deep_branch)
+deep_branch = Dropout(0.5)(deep_branch)
+
+final_branch = concatenate([wide_branch, deep_branch],name='concat_deep_wide')
+final_branch = Dense(units=1,activation='sigmoid',name='combined')(final_branch)
+
+model = Model(inputs=all_inputs, outputs=final_branch)
+
+
+
+
+
+
+
+
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+
+# you will need to install pydot properly on your machine to get this running
+SVG(model_to_dot(model).create(prog='dot', format='svg'))
+
+
+model.compile(optimizer='adagrad',
+              loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.summary()
+# lets also add the history variable to see how we are doing
+# and lets add a validation set to keep track of our progress
+history = model.fit(X_ints_train+ [X_train_num],
+                    y_train,
+                    epochs=10,
+                    batch_size=32,
+                    verbose=1,
+                    validation_data = (X_ints_test + [X_test_num], y_test))
+
+yhat = np.round(model.predict(X_ints_test + [X_test_num]))
+print(mt.confusion_matrix(y_test,yhat), mt.accuracy_score(y_test,yhat))
+
+from matplotlib import pyplot as plt
+
+%matplotlib inline
+
+plt.figure(figsize=(10,4))
+plt.subplot(2,2,1)
+plt.plot(history.history['acc'])
+
+plt.ylabel('Accuracy %')
+plt.title('Training')
+plt.subplot(2,2,2)
+plt.plot(history.history['val_acc'])
+plt.title('Validation')
+
+plt.subplot(2,2,3)
+plt.plot(history.history['loss'])
+plt.ylabel('MSE Training Loss')
+plt.xlabel('epochs')
+
+plt.subplot(2,2,4)
+plt.plot(history.history['val_loss'])
+plt.xlabel('epochs')
+
+
+
+
+
+
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, make_scorer
+from sklearn.model_selection import StratifiedKFold
+
+
+
+#select cross validation
+cv = StratifiedKFold(n_splits=10)
+# select evaluation criteria
+my_scorer = make_scorer(recall_score, average="macro")
+# run model training and cross validation
+per_fold_eval_criteria = cross_val_score(estimator=model, X = X_ints_train+ X_train_num, y = y_train, cv=cv, scoring=my_scorer)
+
+plt.bar(range(len(per_fold_eval_criteria)),per_fold_eval_criteria)
+plt.ylim([min(per_fold_eval_criteria)-0.01,max(per_fold_eval_criteria)])
+plt.show()
+
+
+
+param_grid = dict(epochs=[10,20,30])
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+grid_result = grid.fit(X, Y)
+
+
+
+
+
+#grid search
+import warnings
+warnings.filterwarnings('ignore')
+
+def score_func(y, y_pred, **kwargs):
+    return recall_score(mapped_attack(y),y_pred, average="macro")
+
+my_scorer = make_scorer(score_func)
+
+vals = { 'n_hidden':10,
+         'C':1e-2, 'epochs':200, 'eta':0.001,
+         'alpha':0.001, 'decrease_const':1e-6, 'minibatches':10,
+         'shuffle':True,'random_state':1, 'layers':4, 'cost':'entr', 'phi': 'lin'}
+
+pipe_nn = Pipeline([('scaling', StandardScaler()),
+                    ('pca', PCA(n_components=5)),
+                    ('clf', MultilayerPerceptron(**vals))])
+param_grid = {
+    'clf__C':[1e-2],
+    'clf__cost':["entr","quad"],
+    'clf__layers':[2,3,4],
+    'clf__phi':["lin", "sig","rilu","silu"],
+    'clf__epochs':[400]
+    }
+
+cv = KFold(n_splits=5)
+
+print(pipe_nn.get_params().keys())
+
+gs = GridSearchCV(pipe_nn, param_grid,scoring=my_scorer, cv=cv, verbose=1)
+
+best_model = gs.fit(X_train, y_train.values)
+
+
+
 
 
 
